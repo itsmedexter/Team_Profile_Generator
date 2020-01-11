9f6d3a3861921be40f70b8a9a4af24802b901b8e
Dexter Valencia <deeside@pacbell.net>
2020-01-11 14:34:03 -0800

    Figuring prompt coding, still not working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // dependency for inquirer npm package
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const convertFactory = require('electron-html-to');
+
 const {Manager, Intern, Engineer} = require('./lib');
 // constructor function for creating employee objects
 function Employee(manager, engineer, intern) {
@@ -36,34 +38,69 @@ var createTeam = function() {
         message: "What is your manager's number?: "
       }
     ]).then(function(answers) {
-      // runs the constructor and places the new player object into the variable player.
-      // turns the offense and defense variables into integers as well with parseInt
-      var manager = new Manager(answers.name, parseInt(answers.managerId), answers.managerEmail, parseInt(answers.managerOffice));
 
-      team.manager = manager//Going forward any new team member team.members.push()
-      // adds a player to the starters array if there are less than five player objects in it.
-      // otherwise adds the newest player object to the subs array
-      if (starters.length < 3) {
-        starters.push(player);
-        team.push(player);
-        console.log(player.name + " added to the starters");
+      var manager = new Manager(answers.name, parseInt(answers.managerId), answers.managerEmail, parseInt(answers.managerOffice));
+            // team.manager = manager
+      //Going forward any new team member team.members.push()?
+      
+      // document.onkeydown = checkKey;
+// function to pick engineer, intern, or none
+      function pickEmployee() {      
+          if (e.keyCode === '38') {
+            // up arrow key Engineer
+            inquirer.prompt([
+              {
+                name: "name",
+                message: "What is your engineer's name?: "
+              }, {
+                name: "engineerId",
+                message: "What is your engineer's id: "
+              }, {
+                name: "engineerEmail",
+                message: "What is your engineer's email?: ",
+              }, {
+                name: "engineerOffice",
+                message: "What is your engineer's GitHub username?: "
+              }
+            ]).then(function(answers) {
+        
+              var engineer = new Engineer(answers.name, parseInt(answers.engineerId), answers.engineerEmail, parseInt(answers.engineerGitHubUser));
+          });
+        }
+          if (e.keyCode == '40') {// down arrow key Intern
+            inquirer.prompt([
+              {
+                name: "name",
+                message: "What is your intern's name?: "
+              }, {
+                name: "internId",
+                message: "What is your intern's id: "
+              }, {
+                name: "internEmail",
+                message: "What is your intern's email?: ",
+              }, {
+                name: "internSchool",
+                message: "What is your intern's school?: "
+              }
+            ]).then(function(answers) {
+        
+              var intern = new intern(answers.name, parseInt(answers.internId), answers.internEmail, parseInt(answers.internSchool));
+          });
+        }
+      
+          {
+        
+      
+          if {//I don't want to add anymore,
+          // print to html?
+        //   var html = generateHtml(res.data);
+        // console.log(html);
+        // writeToFile(html);
+        // return html
+        });
       }
-      else {
-        subs.push(player);
-        team.push(player);
-        console.log(player.name + " added to the subs");
-      }
-      // runs the createPlayer function once more
-      createPlayer();
-    });
-  }
-  else {
-    // loops through the team array and calls printStats() for each object it contains
-    for (var i = 0; i < team.length; i++) {
-      team[i].printStats();
     }
-  }
-};
+
 
 // calls the function createEmploye() to start the code
 createEmployee();
